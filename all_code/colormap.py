c9d23ca185f32380e3fec56010b689fd2ebadbc1
motoki <kamimura098@gmail.com>
2025-05-15 17:53:50 +0900

    del all_code folder

--- all_code/colormap.py
+++
@@ -1,7 +0,0 @@
-import cv2
-
-
-def get_colormap_code(name: str) -> int:
-    if hasattr(cv2, f"COLORMAP_{name.upper()}"):
-        return getattr(cv2, f"COLORMAP_{name.upper()}")
-    raise ValueError(f"Invalid colormap name: {name}")
--- all_code/cpu_core.py
+++
@@ -1,2 +0,0 @@
-import multiprocessing
-print("論理CPU数:", multiprocessing.cpu_count())
